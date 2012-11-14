7b6f11b4c5f950f7b60a0f3191b0df704225a626
Scott Smerchek <scott.smerchek@softekinc.com>
2012-11-14 08:14:38 -0600

    Initial attempt at windows support.

--- manifests/init.pp
+++ manifests/init.pp
@@ -28,19 +28,42 @@ class zabbixagent(
     $hostname_real = $hostname
   }
 
-  package {'zabbix-agent' :
-    ensure  => installed,
-  }
+  case $::operatingsystem {
+    debian, ubuntu, centos: {
+      package {'zabbix-agent' :
+        ensure  => installed,
+      }
 
-  service {'zabbix-agent' :
-    ensure  => running,
-    enable  => true,
-    require => Package['zabbix-agent'],
-  }
+      service {'zabbix-agent' :
+        ensure  => running,
+        enable  => true,
+        require => Package['zabbix-agent'],
+      }
 
-  file {'/etc/zabbix/zabbix_agentd.conf' :
-    content => template('zabbixagent/zabbix_agentd.conf.unix.erb'),
-    require => Package['zabbix-agent'],
-    notify  => Service['zabbix-agent'],
+      file {'/etc/zabbix/zabbix_agentd.conf' :
+        content => template('zabbixagent/zabbix_agentd.conf.unix.erb'),
+        require => Package['zabbix-agent'],
+        notify  => Service['zabbix-agent'],
+      }   
+    }
+    windows: {
+      $confdir = "C:/ProgramData/Zabbix"
+      $homedir = "C:/Program Files/Zabbix/"
+
+      file { $confdir: ensure => directory }
+      file { "${confdir}/zabbix_agentd.conf":
+        content => template('zabbixagent/zabbix_agentd.conf.windows.erb'),
+      }
+      file { $homedir: 
+        ensure  => directory,
+        source  => "puppet:///modules/zabbixagent/win64/",
+      }
+
+      exec { 'install Zabbix Agent': 
+        path => $homedir,
+        command => "zabbix_agentd.exe --config ${confdir}/zabbix_agentd.conf --install",
+      }
+    }
+    default: { notice "Unsupported operatingsystem  ${::operatingsystem}" }
   }
 }
