1cb0648ab04ed0b02e5d073568c046524fdb11d2
Gene Liverman <gliverma@westga.edu>
2015-01-17 18:24:42 -0500

    Added selector for the service's name since it's different on Windows.

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,7 +1,13 @@
 # Makes sure the service is running
 class zabbixagent::service {
+  $service_name = $::kernel ? {
+    'Windows' => 'Zabbix Agent',
+    default   => 'zabbix-agent',
+  }
+  
   service { 'zabbix-agent':
     ensure  => running,
+    name    => $service_name,
     enable  => true,
     require => Package['zabbix-agent'],
   }
